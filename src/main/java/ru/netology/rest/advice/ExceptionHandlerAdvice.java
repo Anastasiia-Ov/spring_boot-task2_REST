a53c7d7fb2fcd461813bd61c2bebf6619a289e1d
Anastasiia Ovechkina <an239669@yandex.com>
2025-09-04 23:09:40 +0700

    implemented ExceptionHandlerAdvice

---
+++ src/main/java/ru/netology/rest/advice/ExceptionHandlerAdvice.java
@@ -0,0 +1,23 @@
+package ru.netology.rest.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.netology.rest.exception.InvalidCredentials;
+import ru.netology.rest.exception.UnauthorizedUser;
+
+@RestControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    @ExceptionHandler(InvalidCredentials.class)
+    public ResponseEntity<String> icHandler(InvalidCredentials e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnauthorizedUser.class)
+    public ResponseEntity<String> uuHandler(UnauthorizedUser e) {
+        System.out.println(e.toString());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}
